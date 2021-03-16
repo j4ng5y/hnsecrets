ac1836ec78875434f7383b96ff27f749498e5d93
Alexandru Bordei <alexb2123@gmail.com>
2021-03-15 23:26:23 -0400

    added some comments to code

--- svcs/secrets/pkg/secrets/secrets/business_logic.go
+++ svcs/secrets/pkg/secrets/secrets/business_logic.go
@@ -9,19 +9,12 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Secret struct {
-	ID     string `json: "id"`
-	Secret string `json: "secret"`
-}
-
 var secrets []Secret
 
 func createSecret() {
 	var secret Secret
-	_ = json.NewDecoder(r.Body).Decode(&secret)
 	secret.ID = strconv.Itoa(rand.Intn(10000000))
 	secrets = append(secrets, secret)
-	json.NewEncoder(w).Encode(secret)
 }
 
 func deleteSecret(w http.ResponseWriter, r *http.Request) {
@@ -29,9 +22,9 @@ func deleteSecret(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for index, item := range secrets {
 		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
+			secrets = append(secrets[:index], secrets[index+1:]...)
 			break
 		}
 	}
-	json.NewEncoder(w).Encode(books)
+	json.NewEncoder(w).Encode(secrets)
 }
